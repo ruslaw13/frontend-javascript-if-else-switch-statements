56c2df7fe67a25bce1d3787812c8f2c0d2ebc830
ruslaw13 <rs3.sebas@gmail.com>
2021-04-06 23:56:56 +0200

    excercise switch-statemenst with bonus is done

--- switch-statements.js
+++ switch-statements.js
@@ -15,7 +15,23 @@ const errorCode = 0;
 // const errorCode = 0 geeft "Geen watertoevoer" in de terminal
 // const errorCode = 4 geeft "Geen druk" in de terminal
 // etc.
-
+switch (errorCode) {
+    case 0:
+        console.log("Geen watertoevoer");
+        break;
+    case 1:
+        console.log("Temperatuur te laag");
+        break;
+    case 2:
+        console.log("Koffiebonen op");
+        break;
+    case 3:
+        console.log("Afvalbak vol");
+        break;
+    case 4:
+        console.log("Geen druk");
+        break;
+}
 
 // ==========================================
 // Opdracht 2: schrijf een switch statement die op basis van de volgende variabele een bijbehorende foutmelding in de console logt.
@@ -35,8 +51,31 @@ const monitorColor = "red";
 // const monitorColor = "red" geeft "Kritieke hartslag, actie vereist!" in de terminal
 // const monitorColor = "green" geeft "Hartslag stabiel" in de terminal
 // etc.
-
+switch (monitorColor) {
+    case "green":
+        console.log("Hartslag stabiel");
+        break;
+    case "yellow":
+        console.log("Hartslag verhoogd");
+        break;
+    case "orange":
+    case "red":
+        console.log("Kritieke hartslag, actie vereist!");
+        break;
+    default:
+        console.log("Status hartslag onbekend");
+}
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: kun je de switch-statement uit opdracht 2 ook opschrijven met een aantal if-statements?
 // ==========================================
+
+if (monitorColor === "green") {
+    console.log("Hartslag stabiel");
+} else if (monitorColor === "yellow") {
+    console.log("Hartslag verhoogd");
+} else if (monitorColor === "red" || monitorColor === "orange") {
+    console.log("Kritieke hartslag, actie vereist!");
+} else {
+    console.log("Status hartslag onbekend");
+}
